d732673c585ecfaea4cc604902f48071c7908eb1
FilipeSouza <filipe.souza1602@gmail.com>
2022-10-21 22:24:11 -0300

    feat: add find many persons

--- src/services/person.ts
+++ src/services/person.ts
@@ -1,8 +1,19 @@
 import { Person } from "../models/person";
 import { Api } from "./api";
 
+type Page<T> = {
+  data: T[]
+  total?: number
+}
+
+export type FindPageResponse = Pick<Person, 'id' | 'name' | 'contact_phone'> 
+
 export class PersonService {
   static async addPerson(person: Omit<Person, "id" | "demission_date">) {
     await new Api().post("/person", person);
+  }
+
+  static async findMany(): Promise<Page<FindPageResponse>> {
+    return await new Api().get("/person");
   }
 }
