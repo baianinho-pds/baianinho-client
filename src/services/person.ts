5f794f2661a11ade9e59741eb299e5b4f210bc7a
Samuel <samuelalves681@gmail>
2022-10-23 23:09:56 -0300

    feat: add delete person on person service

--- src/services/person.ts
+++ src/services/person.ts
@@ -2,11 +2,11 @@ import { Person } from "../models/person";
 import { Api } from "./api";
 
 type Page<T> = {
-  data: T[]
-  total?: number
-}
+  data: T[];
+  total?: number;
+};
 
-export type FindPageResponse = Pick<Person, 'id' | 'name' | 'contact_phone'> 
+export type FindPageResponse = Pick<Person, "id" | "name" | "contact_phone">;
 
 export class PersonService {
   static async addPerson(person: Omit<Person, "id" | "demission_date">) {
@@ -18,10 +18,14 @@ export class PersonService {
   }
 
   static async findOne(id: number): Promise<Person> {
-    return await new Api().get(`/person/${id}`)
+    return await new Api().get(`/person/${id}`);
   }
 
-  static async updatePerson(id: number, person: Omit<Person, 'id'>) {
-    await new Api().put(`/person/${id}`, person)
+  static async updatePerson(id: number, person: Omit<Person, "id">) {
+    await new Api().put(`/person/${id}`, person);
+  }
+
+  static async deletePerson(id: number) {
+    await new Api().delete(`/person/${id}`);
   }
 }
