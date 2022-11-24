aee400de80b33c52b28796872abd7216af8f77b2
FilipeSouza <filipe.souza1602@gmail.com>
2022-11-23 21:47:59 -0300

    feat: add route of materia prima

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
+import MateriaPrima from "./pages/MateriaPrima";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/materia-prima" element={<MateriaPrima />} />
         </Routes>
       </div>
     </BrowserRouter>
