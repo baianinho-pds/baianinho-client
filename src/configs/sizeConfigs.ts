57ffed24abf154ae1c094c9d645588e0e35433a2
FilipeSouza <filipe.souza1602@gmail.com>
2022-11-26 14:35:18 -0300

    feat: create size configs of sidebar

---
+++ src/configs/sizeConfigs.ts
@@ -0,0 +1,11 @@
+const sizeConfigs = {
+  sizebar: {
+    width: '250px',
+    fontSize: '22px',
+    marginTop: '80px',
+    paddingY: '18px',
+    paddingX: '24px'
+  }
+}
+
+export default sizeConfigs;
