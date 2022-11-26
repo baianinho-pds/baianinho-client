1980b829bd5fb98de22b05f5c82f58c60f285f84
FilipeSouza <filipe.souza1602@gmail.com>
2022-11-26 14:35:31 -0300

    feat: create color configs of sidebar

---
+++ src/configs/colorConfigs.ts
@@ -0,0 +1,12 @@
+import { colors } from '@mui/material'
+
+const colorConfigs = {
+  sidebar: {
+    bg: '#315d9f',
+    color: '#eeeeee',
+    hoverBg: '#2b518a',
+  },
+  mainBg: colors.grey["100"]
+}
+
+export default colorConfigs
