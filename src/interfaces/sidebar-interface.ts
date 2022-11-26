d6d9a9a21bb2ac5b112abef58657d56893cd996b
FilipeSouza <filipe.souza1602@gmail.com>
2022-11-26 14:30:54 -0300

    feat: create sidebar parameter types

---
+++ src/interfaces/sidebar-interface.ts
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export type ItemWithChildren = {
+  titlePage: string;
+  iconPage?: ReactNode;
+  linkPage: string;
+  children?: ItemWithoutChildren[];
+};
+
+export type ItemWithoutChildren = {
+  titlePage: string;
+  linkPage: string;
+};
