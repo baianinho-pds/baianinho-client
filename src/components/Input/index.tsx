736330dda7d1e4fd6892b3e6f3c404427ac26433
Samuel <samuelalves681@gmail>
2022-12-10 22:28:46 -0300

    feat: Adicionado component de input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import ReactInputMask from "react-input-mask";
+import styles from "./input.module.css";
+
+interface InputProps extends Partial<HTMLInputElement> {
+  label?: string;
+  mask?: string;
+  onChangeInputValue?: (value: string) => void;
+}
+
+export function Input({
+  label,
+  type,
+  name,
+  id,
+  placeholder,
+  value,
+  mask,
+  minLength,
+  maxLength,
+  onChangeInputValue,
+}: InputProps) {
+  const [inputValue, setInputValue] = useState(value || "");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    onChangeInputValue && onChangeInputValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.containerInput}>
+      <label htmlFor={id || name}>{label}</label>
+      <>
+        {mask ? (
+          <ReactInputMask
+            mask={mask}
+            type={type}
+            name={name}
+            id={id || name}
+            placeholder={placeholder}
+            minLength={minLength}
+            maxLength={maxLength}
+            value={inputValue}
+            onChange={(e) => onChange(e)}
+          />
+        ) : (
+          <input
+            type={type}
+            name={name}
+            id={id || name}
+            placeholder={placeholder}
+            minLength={minLength}
+            maxLength={maxLength}
+            value={inputValue}
+            onChange={(e) => onChange(e)}
+          />
+        )}
+      </>
+    </div>
+  );
+}
